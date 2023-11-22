07c4126c014168124d626c9802a6ca9d86ee100f
james <jimmindungu3@gmail.com>
2023-11-22 18:15:09 +0300

    doner

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,34 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    countdown = 10
+    
+    while countdown > 0:
+        print(countdown)
+        countdown -= 1
+    
+    print("Happy New Year!")
+    
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    square_intagers = [int * int for int in int_list]
+    print(square_intagers)
+    return square_intagers
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    counter = 1
+
+    while counter <= 100:
+        if counter % 3 == 0 and counter % 5 == 0:
+            print("FizzBuzz")
+            counter += 1
+        elif counter % 3 == 0:
+            print("Fizz")
+            counter += 1
+        elif counter % 5 == 0:
+            print("Buzz")
+            counter += 1
+        else:
+            print(counter)
+            counter += 1
+
